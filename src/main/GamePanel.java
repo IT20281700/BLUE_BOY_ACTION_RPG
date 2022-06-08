44761cea2abebd01b371165b4d00aa4bdb953765
IT20281700 <it20281700@my.sliit.lk>
2022-06-08 15:19:25 +0530

    add sound effects

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -31,17 +31,20 @@ public class GamePanel extends JPanel implements Runnable {
 	// WORLD MAP SETTINGS
 	public final int maxWorldCol = 50;
 	public final int maxWorldRow = 50;
-	public final int worldWidth = tileSize * maxScreenCol;
-	public final int worldHeight = tileSize * maxScreenRow;
 	
 	// FPS
 	int FPS = 60;
 	
+	// SYSTEM
 	public TileManager tileManager = new TileManager(this);
 	KeyHandler keyH = new KeyHandler(this);
-	Thread gameThread;
+	Sound sound = new Sound();
 	public CollisionChecker cChecker = new CollisionChecker(this);
 	public AssetSetter aSetter = new AssetSetter(this);
+	Thread gameThread;
+	
+	
+	// ENTITY AND OBJECT
 	public Player player = new Player(this,keyH);
 	public SuperObject obj[] = new SuperObject[10];
 	
@@ -58,6 +61,7 @@ public class GamePanel extends JPanel implements Runnable {
 	public void setupGame() {
 		
 		aSetter.setObject();
+		playMusic(0);
 		
 	}
 	
@@ -179,6 +183,27 @@ public class GamePanel extends JPanel implements Runnable {
 		player.draw(g2);
 		
 		g2.dispose();
+		
+	}
+	
+	public void playMusic(int i) {
+		
+		sound.setFile(i);
+		sound.play();
+		sound.loop();
+		
+	}
+	
+	public void stopMusic() {
+		
+		sound.stop();
+		
+	}
+	
+	public void playSE(int i) {
+		
+		sound.setFile(i);
+		sound.play();
 		
 	}
 	
